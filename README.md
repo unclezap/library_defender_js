5fd657ae92c930c4a9c358126bf11511995c97f3
unclezap <zeb.blackwell@gmail.com>
2020-03-23 15:45:01 -0700

    bare bones

---
+++ README.md
@@ -0,0 +1,1 @@
+LIBRARY DEFENDER is an amazing game which you will want to share with all your friends and is very cool and which shows off all of our l33t coding skillz
---
+++ src/index.js
@@ -0,0 +1,15 @@
+//started on the script
+
+document.addEventListener("DOMContentLoaded", function() {
+    const newPlayer = document.getElementById("createAccountButton")
+    const oldPlayer = document.getElementById("WHATEVERTHENAMEWAS")
+  
+    newPlayer.addEventListener("submit", function() {
+        createAccount()
+    })
+  
+  })
+  
+  function createAccount () {
+  fetch something
+  }
