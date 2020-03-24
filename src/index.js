bd9d298ee1e9277d3b56d61d38e44b4c3477a3b3
unclezap <zeb.blackwell@gmail.com>
2020-03-24 15:23:47 -0700

    Initial user fetches working

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
+const usersURL = 'http://localhost:3000/users'
+
 document.addEventListener("DOMContentLoaded", function() {
     const newPlayer = document.getElementById("createAccountButton")
     const oldPlayer = document.getElementById("signInButton")
-    
-    console.log(newPlayer)
-    console.log("hi")
     
     newPlayer.addEventListener("click", function() {
         event.preventDefault()
@@ -12,27 +11,54 @@ document.addEventListener("DOMContentLoaded", function() {
   
     oldPlayer.addEventListener("click", function(event) {
         event.preventDefault()
-        findUser(event)
+        getUsers(event)
     })
   })
   
   function createUser () {
-  console.log("You're making a new player! Huzzah!")
-  }
-
-  function findUser (userId) {
-    console.log(userId)
-    console.log("You're finding old games!")
-    //need to see what info we get back and set a variable to user_id
-    fetch(`http://localhost:3000/users/${userIdVariableWeSetEarlier}`)
+    newUser = {name: "Testy McTesterson"}
+    console.log("You're making a new player! Huzzah!")
+    fetch(usersURL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            newUser
+        })      
+    })
     .then(function(response) {
         return response.json()
     })
     .then(function(data) {
-        showGames(data)
+        console.log(data)
+        debugger;
+    })
+    .catch((error) => {
+        alert ("OH FUCK");
+        console.error('Error:', error)
+    })
+  }
+
+  function getUsers (userId) {
+    console.log(userId)
+    console.log("You're finding old games!")
+    //need to see what info we get back and set a variable to user_id
+    fetch(usersURL)
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(data) {
+        console.log(data)
+    })
+    .catch((error) => {
+        alert ("OH FUCK");
+        console.error('Error:', error)
     })
   }
 
   function showGames (gameData) {
       console.log("this makes a ul that lists all of the user's games that you can click on to load that game")
+      console.log
   }
