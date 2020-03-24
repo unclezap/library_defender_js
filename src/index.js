7ed7608eb1e34da28cb1533e8f9046d558c85b75
AidanLincoln <aidantlincoln@gmail.com>
2020-03-24 16:35:59 -0700

    feature => master css, event listeners, dynamic buttons

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
 document.addEventListener("DOMContentLoaded", function() {
     const newPlayer = document.getElementById("createAccountButton")
     const oldPlayer = document.getElementById("signInButton")
-    
+    const signOut = document.getElementById('signOutButton')
+    const newGame = document.getElementById('newGameButton')
+    const oldGame = document.getElementById('oldGameButton')
+
+    const bigLogo = document.getElementById('bigLogoDiv')
+
+
     console.log(newPlayer)
     console.log("hi")
     
     newPlayer.addEventListener("click", function() {
         event.preventDefault()
+        const contentBox = document.getElementById('bigbox')
+        contentBox.className = "contentbox"
+        const newUserForm = document.getElementById('newUserForm')
+        bigLogo.hidden = true
+        newUserForm.addEventListener('submit', function(event){
+            event.preventDefault()
+
+            //use event attributes to create new user
+            
+            contentBox.className = "hiddencontentbox"
+            newPlayer.hidden = true
+            oldPlayer.hidden = true
+            signOut.hidden = false
+            newGame.hidden = false
+            oldGame.hidden = false
+        })
         createUser()
     })
   
     oldPlayer.addEventListener("click", function(event) {
         event.preventDefault()
+        const contentBox = document.getElementById('bigbox')
+        contentBox.className = "contentbox"
         findUser(event)
     })
   })
