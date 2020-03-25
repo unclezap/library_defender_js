3954c2adda77167b90d25ce2ea5ca64e6f8e8c97
unclezap <zeb.blackwell@gmail.com>
2020-03-25 16:30:17 -0700

    not much change

--- src/modules/gamePlay.js
+++ src/modules/gamePlay.js
@@ -1,1 +1,23 @@
 console.log("The gameplay is running!!!!")
+const usersURL = 'http://localhost:3000/users'
+const gamesURL = 'http://localhost:3000/games'
+const levelsURL = 'http://localhost:3000/levels'
+const librariesURL = 'http://localhost:3000/levels'
+const 
+
+const currentLevel
+const currentHealth
+const currentMoney
+
+function playGame(thisUser, thisGame) {
+    levelFetch(thisUser)
+    .then
+}
+
+function levelFetch(thisUser) {
+    fetch()
+}
+
+//fetch for level
+
+//fetch for monsters from level
